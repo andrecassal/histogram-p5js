224136b7234a2cb50ed8c9aa4ca06ba24e500a6f
Andre Cassal <andrecassal@users.noreply.github.com>
2020-08-15 22:34:33 -0600

    Creating avg() and moving variables to properties

--- Histogram.js
+++ Histogram.js
@@ -14,6 +14,8 @@ class Histogram{
 		this.total = 0;
 		this.varianceR = this.varianceG = this.varianceB = 0;
 		this.meanR = this.meanG = this.meanB = 0;
+		this.totalR = this.totalG = this.totalB = 0;
+		this.valueR = this.valueG = this.valueB = 0;
 		this.stdDeviation = 0;
 		this.error = 0;
 		this.area_exponent = 0.25
@@ -30,47 +32,57 @@ class Histogram{
 	rgb(){
 		return [this.R, this.G, this.B];
 	}
-	weightedAvg(){
-
-		let totalR,totalG,totalB;
-		let valueR,valueG,valueB;
-		totalR = totalG = totalB = 0;
-		valueR = valueG = valueB = 0;
-
-		for(let i=0;i < 256;i++){
-			totalR += this.R[i];
-			totalG += this.G[i];
-			totalB += this.B[i];
-			//sum weighted 
-			valueR += this.R[i] * i;
-			valueG += this.G[i] * i;
-			valueB += this.B[i] * i;
-		}
-		//True mean
-		this.meanR = totalR / this.R.length;
-		this.meanG = totalG / this.G.length;
-		this.meanB = totalB / this.B.length;
+	avg(){
 		
-		//what is this?
-		valueR /= totalR;
-		valueG /= totalG;
-		valueB /= totalB;
+		for(let i=0;i < 256;i++){
+			this.totalR += this.R[i];
+			this.totalG += this.G[i];
+			this.totalB += this.B[i];
+			//sum weighted 
+			this.valueR += this.R[i] * i;
+			this.valueG += this.G[i] * i;
+			this.valueB += this.B[i] * i;
+		}
+		
+		//True mean
+		this.meanR = this.totalR / this.R.length;
+		this.meanG = this.totalG / this.G.length;
+		this.meanB = this.totalB / this.B.length;
+		
+		// //what is this?
+		this.valueR /= totalR;
+		this.valueG /= totalG;
+		this.valueB /= totalB;
+				
+	}
+	weightedAvg(){
 
 	// weightedAvg
 		for(let i=0;i < 256;i++){
-			this.varianceR += sq( valueR - i ) * this.R[i];
-			this.varianceG += sq( valueG - i ) * this.G[i];
-			this.varianceB += sq( valueB - i ) * this.B[i];
+			this.varianceR += sq( avg.valueR - i ) * this.R[i];
+			this.varianceG += sq( avg.valueG - i ) * this.G[i];
+			this.varianceB += sq( avg.valueB - i ) * this.B[i];
 		}
-		this.varianceR = Math.sqrt(this.varianceR / totalR);
-		this.varianceB = Math.sqrt(this.varianceB / totalB);
-		this.varianceG = Math.sqrt(this.varianceG / totalG);
 		
-		this.total = (totalR + totalG + totalB) / 3;
+		this.varianceR = Math.sqrt(this.varianceR / this.totalR);
+		this.varianceB = Math.sqrt(this.varianceB / this.totalB);
+		this.varianceG = Math.sqrt(this.varianceG / this.totalG);
+		
+		this.total = (this.totalR + this.totalG + this.totalB) / 3;
 		this.variance = this.varianceR * 0.2989 + this.varianceG * 0.5870 + this.varianceB * 0.1140;
 		this.stdDeviation = Math.sqrt( this.variance );
 		this.error = this.stdDeviation * this.area_error;
 		
-		return [valueR,valueG,valueB, this.error ]; 
+		return [this.valueR,
+						this.valueG,
+						this.valueB, 
+						this.error ]; 
 	}
 }
+
+
+
+
+
+
+
